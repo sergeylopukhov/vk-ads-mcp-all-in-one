a40bf460080b9baf12927641de1702ba8642f38e
Sergey Lopukhov <sergey.lopukhov@gmail.com>
2026-07-29 22:50:40 +0300

    Release 0.1.0 beta 1

--- src/server.ts
+++ src/server.ts
@@ -171,7 +171,7 @@ import { actionReadinessSchema } from "./preflight/types.js";
 
 export const SERVER_INFO = {
   name: "vk-ads-mcp",
-  version: "0.6.0",
+  version: "0.1.0-beta.1",
 } as const;
 
 export const CONNECTION_CHECK_TOOL = "vk_ads_connection_check";
